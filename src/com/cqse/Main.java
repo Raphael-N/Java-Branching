50adbc75efca8ac83b04f78a2e3e34815726301a
Raphael Nömmer <R.Noemmer@gmx.de>
2020-09-18 14:08:14 +0200

    Test 1 (#1)
    * add findings
    
    * add more
    
    Co-authored-by: Raphael Nömmer <noemmer@cqse.eu>

--- src/com/cqse/Main.java
+++ src/com/cqse/Main.java
@@ -2,7 +2,21 @@ package com.cqse;
 
 public class Main {
 
+    public String test;
+
+    //TODO do something!
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    private String blub() {
+        int TEST_VALUE = 5;
+        return "Blubba";
+    }
+
+    private void blob() {
+        String woah = "wuha";
+        if(5 == 3)
+            woah.length();
     }
 }
