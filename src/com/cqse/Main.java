4a3902f8fb9b5e08526819296c09ed4d098127d0
Raphael Nömmer <noemmer@cqse.eu>
2020-09-18 13:50:12 +0200

    add findings

--- src/com/cqse/Main.java
+++ src/com/cqse/Main.java
@@ -2,7 +2,15 @@ package com.cqse;
 
 public class Main {
 
+    public String test;
+
+    //TODO do something!
     public static void main(String[] args) {
 	// write your code here
+    }
+
+    private String blub() {
+        int TEST_VALUE = 5;
+        return "Blubba";
     }
 }
